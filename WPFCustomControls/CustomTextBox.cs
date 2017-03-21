1fb29452f22452f6a513732541f0e6a174480baa
yeffySelf <18811505262@163.com>
2017-03-21 15:57:30 +0800

    CustomTextBox加入水印功能

--- WPFCustomControls/CustomTextBox.cs
+++ WPFCustomControls/CustomTextBox.cs
@@ -4,37 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace WPFCustomControls
 {
     public class CustomTextBox:TextBox
     {
+        private CustomTextBoxAdorner _adorner = null;
         public CustomTextBox()
         {
             
         }
 
-        public static readonly DependencyProperty AllowWaterMarkProperty = DependencyProperty.Register("AllowWaterMark", typeof(bool), typeof(CustomTextBox), new PropertyMetadata(false, OnAllowWaterMarkChanged));
-
-        private static void OnAllowWaterMarkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            CustomTextBox ctb = d as CustomTextBox;
-            ctb.SetWaterMark((bool)e.NewValue, ctb.WaterMark ?? "");
-        }
-
-        public bool AllowWaterMark
-        {
-            get { return (bool)GetValue(AllowWaterMarkProperty); }
-            set { SetValue(AllowWaterMarkProperty,value); }
-        }
-
-        public static readonly DependencyProperty WaterMarkProperty = DependencyProperty.Register("WaterMark", typeof(string), typeof(CustomTextBox),new PropertyMetadata("",OnWaterMarkChanged));
-
-        private static void OnWaterMarkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            CustomTextBox ctb = d as CustomTextBox;
-            ctb.SetWaterMark(ctb.AllowWaterMark, e.NewValue ?? "");
-        }
+        public static readonly DependencyProperty WaterMarkProperty = DependencyProperty.Register("WaterMark", typeof(string), typeof(CustomTextBox),new PropertyMetadata(""));
 
         public string WaterMark
         {
@@ -42,11 +26,61 @@ namespace WPFCustomControls
             set { SetValue(WaterMarkProperty, value); }
         }
 
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            SetWaterMark();
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            SetWaterMark();
+        }
+
         #region internal method
 
-        private void SetWaterMark(bool allowWaterMark,object waterMark)
+        private void SetWaterMark()
         {
-
+            AdornerLayer layer = AdornerLayer.GetAdornerLayer(this);
+            if (string.IsNullOrWhiteSpace(this.Text) && !string.IsNullOrWhiteSpace(this.WaterMark))
+            {
+                //添加装饰器
+                if (_adorner == null)
+                {
+                    _adorner = new CustomTextBoxAdorner(this);
+                }
+                
+                bool alreadyAdded = false;
+                Adorner[] adorners = layer.GetAdorners(this);
+                if (adorners != null)
+                {
+                    foreach (var adorner in adorners)
+                    {
+                        if (adorner is CustomTextBoxAdorner)
+                        {
+                            alreadyAdded = true;
+                        }
+                    }
+                }
+                if (!alreadyAdded)
+                    layer.Add(_adorner);
+            }
+            else
+            {
+                //移除已添加的装饰器
+                Adorner[] adorners = layer.GetAdorners(this);
+                if (adorners != null)
+                {
+                    foreach (var adorner in adorners)
+                    {
+                        if (adorner is CustomTextBoxAdorner)
+                        {
+                            layer.Remove(adorner);
+                        }
+                    }
+                }
+            }
         }
 
         #endregion
---
+++ WPFCustomControls/CustomTextBoxAdorner.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WPFCustomControls
+{
+    public class CustomTextBoxAdorner : Adorner
+    {
+        private readonly VisualCollection _collection;
+
+        private readonly CustomTextBox _textBox;
+
+        private readonly TextBlock _textBlock;
+        public CustomTextBoxAdorner(CustomTextBox adornedElement) : base(adornedElement)
+        {
+            _collection = new VisualCollection(this);
+            _textBlock = new TextBlock()
+            {
+                FontSize = adornedElement.FontSize,
+                FontFamily = adornedElement.FontFamily,
+                FontStyle = adornedElement.FontStyle,
+                FontWeight = adornedElement.FontWeight,
+                VerticalAlignment = adornedElement.VerticalContentAlignment,
+                Foreground = new SolidColorBrush(Colors.Gray),
+                Margin = new Thickness(3,0,0,0),
+                Cursor = System.Windows.Input.Cursors.IBeam
+            };
+            _textBlock.SetBinding(TextBlock.TextProperty, new Binding("WaterMark") { Source = adornedElement });
+            _textBox = adornedElement;
+            _collection.Add(_textBlock);
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return _collection.Count;
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _collection[index];
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _textBlock.Arrange(new Rect(0, 0, _textBox.DesiredSize.Width, _textBox.DesiredSize.Height));
+            return finalSize;
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            _textBox.Focus();
+        }
+    }
+}
